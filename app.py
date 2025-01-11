bbacc02cd9c0cfa60481b9c4578d2ac3b852fa77
AlexL <alexlizarazo@gmail.com>
2025-01-11 18:51:33 -0500

    Terminado version 1.0

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
 import os
+import threading
 from pydub import AudioSegment
 from pydub.silence import detect_nonsilent
 import whisper
 import time
-from tkinter import Tk
+import tkinter as tk
 from tkinter.filedialog import askopenfilename
 import warnings
+from scipy.io import wavfile
+import noisereduce as nr
+from tkinter import messagebox
 
 
 def rename_file(file_path):
@@ -59,15 +63,44 @@ def audio_to_mp3(audio_path):
         print("No convirtio nada")
     return "ERROR"
 
-def main():
+"""
+#ELIMINA EL RUIDO PERO HACE QUE NO DETECTE BIEN LAS PALABRAS
+def eliminar_ruido(audio_segment):
+    # Exportar el segmento de audio a un archivo WAV temporal
+    audio_segment.export("temp.wav", format="wav")
+
+    # Leer el archivo WAV temporal
+    rate, data = wavfile.read("temp.wav")
+
+    # Reducir el ruido
+    reduced_noise = nr.reduce_noise(y=data, sr=rate)
+
+    # Guardar el archivo WAV con ruido reducido
+    wavfile.write("temp_reduced.wav", rate, reduced_noise)
+
+    # Cargar el archivo WAV con ruido reducido como un segmento de audio
+    audio_reduced = AudioSegment.from_wav("temp_reduced.wav")
+
+    # Eliminar archivos temporales
+    os.remove("temp.wav")
+    os.remove("temp_reduced.wav")
+
+    return audio_reduced
+"""
+
+
+
+# Función que será ejecutada al presionar el botón
+def transcribir():
     start_time = time.time()
     # Cargar el archivo de audio
     audio_path = get_audiofile()
-    
     audio_path_mp3= audio_to_mp3(audio_path)
-
     audio = AudioSegment.from_file(audio_path_mp3)
 
+    # Eliminar el ruido ambiental
+    #audio = eliminar_ruido(audio)
+    
     # Detectar las partes donde hay audio (en milisegundos)
     nonsilent_ranges = detect_nonsilent(audio, min_silence_len=1000, silence_thresh=-40)
 
@@ -93,7 +126,7 @@ def main():
         audio_chunk.export("temp.wav", format="wav")
 
         # Transcribir
-        result = model.transcribe("temp.wav")
+        result = model.transcribe("temp.wav", language="es")
         transcriptions.append(result["text"])
 
         # Mostrar progreso manual
@@ -109,18 +142,61 @@ def main():
     temp_file = "temp.wav"
     if os.path.exists(temp_file):
         os.remove(temp_file)
-        print(f"Archivo temporal '{temp_file}' borrado.")
+        #print(f"Archivo temporal '{temp_file}' borrado.")
 
     # Borrar el archivo temporal
     audio_temp_file = "audioTemporal.mp3"
     if os.path.exists(audio_temp_file):
         os.remove(audio_temp_file)
-        print(f"Archivo temporal '{audio_temp_file}' borrado.")
+        #print(f"Archivo temporal '{audio_temp_file}' borrado.")
     end_time = time.time()
 
     execution_time = end_time - start_time
 
     print(f"Tiempo total de ejecución: {execution_time} segundos")
+    #label.config(text=f"Transcripción final: {transcripcion_final}")
+    messagebox.showinfo("",transcripcion_final)
+
+
+
+def main():
+    global label
+    # Crear la ventana principal
+    root = tk.Tk()
+
+    # Configurar la ventana principal
+    root.title("")  # Título de la ventana
+    window_width = 300  # Ancho de la ventana
+    window_height = 200  # Alto de la ventana
+
+    # Obtener el tamaño de la pantalla
+    screen_width = root.winfo_screenwidth()
+    screen_height = root.winfo_screenheight()
+
+    # Calcular las coordenadas x e y para centrar la ventana
+    x_coordinate = (screen_width // 2) - (window_width // 2)
+    y_coordinate = (screen_height // 2) - (window_height // 2)
+
+    # Configurar la geometría de la ventana
+    root.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
+
+    # Opcional: evitar redimensionar la ventana
+    root.resizable(False, False)
+
+    # Agregar widgets
+    label = tk.Label(root, text="Bienvenido al transcriptor!")  # Crear una etiqueta
+    label.pack(pady=20)  # Mostrar la etiqueta con un espacio de 20 píxeles
+
+    button_transcriptor = tk.Button(root, text="Elegir audio", command=transcribir)  # Crear un botón para cerrar la ventana
+    button_transcriptor.pack(pady=10)
+    button = tk.Button(root, text="Cerrar", command=root.destroy)  # Crear un botón para cerrar la ventana
+    button.pack(pady=10)
+
+    # Iniciar el bucle principal de la aplicación
+    root.mainloop()
+    
+    return
+    
 
 if __name__ == "__main__":
     main()
---
+++ run.bat
@@ -0,0 +1,6 @@
+@echo off
+REM Activar el entorno virtual
+call .venv\Scripts\activate
+
+REM Ejecutar el archivo app.py
+python app.py
