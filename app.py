5c03299722b43fac64d0a993d77e18dd8e0a0631
AlexL <alexlizarazo@gmail.com>
2025-01-18 17:39:09 -0500

    CODIGO LIMPIO

--- app.py
+++ app.py
@@ -1,18 +1,9 @@
 import os
-import threading
 from pydub import AudioSegment
-from pydub.silence import detect_nonsilent
 import whisper
 import time
-import tkinter as tk
 from tkinter.filedialog import askopenfilename
 import warnings
-from scipy.io import wavfile
-import noisereduce as nr
-from tkinter import messagebox
-import torch
-from pathlib import Path
-
 
 def rename_file(file_path):
     if file_path.endswith(".dat.unknown"):
@@ -100,7 +91,6 @@ def eliminar_ruido(audio_segment):
 """
 
 
-
 # Función que será ejecutada al presionar el botón
 def transcribir():
 
@@ -111,32 +101,18 @@ def transcribir():
 
     start_time = time.time()
     audio_path_mp3= audio_to_mp3(audio_path)
-    print(audio_path_mp3)
-    #audio = AudioSegment.from_file(audio_path_mp3)
-    #print(torch.cuda.is_available())  # Esto debe devolver True.
-    #print(torch.cuda.get_device_name(0))
 
-    # Detectar las partes donde hay audio (en milisegundos)
-    #nonsilent_ranges = detect_nonsilent(audio, min_silence_len=1000, silence_thresh=-40)
-
-    # Mostrar los intervalos con audio
-    #print("Rangos de audio detectados:", nonsilent_ranges)
+    #LOS SILENCIOS POR AHORA MANUAL EN AUDACITY
 
     # Suprimir advertencias específicas
-    #warnings.filterwarnings("ignore", category=FutureWarning)
-    #warnings.filterwarnings("ignore", message="FP16 is not supported on CPU; using FP32 instead")
+    warnings.filterwarnings("ignore", category=FutureWarning)
+    warnings.filterwarnings("ignore", message="FP16 is not supported on CPU; using FP32 instead")
 
     # Cargar el modelo de Whisper
-    #model = whisper.load_model("base", device="cuda")
     model = whisper.load_model("turbo", device="cuda")
-    #model = whisper.load_model("large")
-    #model = whisper.load_model("medium")
-    
-    # Exportar el audio completo a un archivo temporal
-        #audio.export("temp.wav", format="wav")
+
     # Transcribir el audio completo
-    #result = model.transcribe("temp.wav", language="es")
-    result = model.transcribe(audio_path_mp3)
+    result = model.transcribe(audio_path_mp3)#, language="es")
     transcripcion_final = result["text"]
     print(f"Transcripción final: \033[1m{transcripcion_final}\033[0m")
 
@@ -158,99 +134,11 @@ def transcribir():
         #print(f"Archivo temporal '{audio_temp_file}' borrado.")
     end_time = time.time()
 
-    return
-
-    # Transcribir solo las partes con voz
-    transcriptions = []
-    total_fragments = len(nonsilent_ranges)
-
-    for i, (start, end) in enumerate(nonsilent_ranges):
-        # Cortar y exportar el fragmento
-        audio_chunk = audio[start:end]
-        audio_chunk.export("temp.wav", format="wav")
-
-        # Transcribir
-        result = model.transcribe("temp.wav")
-        #result = model.transcribe("temp.wav", language="es")
-        transcriptions.append(result["text"])
-
-        # Mostrar progreso manual
-        progress = ((i + 1) / total_fragments) * 100
-        print(f"Progreso: {progress:.2f}% ({i + 1}/{total_fragments} fragmentos procesados)")
-
-    textos_filtrados = [texto for texto in transcriptions if texto.strip()]
-    # Imprimir la transcripción combinada
-    transcripcion_final = " ".join(textos_filtrados)
-    print(f"Transcripción final: \033[1m{transcripcion_final}\033[0m")
-
-    # Borrar el archivo temporal
-    temp_file = "temp.wav"
-    if os.path.exists(temp_file):
-        os.remove(temp_file)
-        #print(f"Archivo temporal '{temp_file}' borrado.")
-
-    # Borrar el archivo temporal
-    audio_temp_file = "audioTemporal.mp3"
-    if os.path.exists(audio_temp_file):
-        os.remove(audio_temp_file)
-        #print(f"Archivo temporal '{audio_temp_file}' borrado.")
-    end_time = time.time()
-
-    execution_time = end_time - start_time
-
-    print(f"Tiempo total de ejecución: {execution_time} segundos")
-    #messagebox.showinfo("",transcripcion_final)
-
-
 
 def main():
 
     transcribir()
 
-
-
-
-
-
-
-    """
-    global label
-    # Crear la ventana principal
-    root = tk.Tk()
-
-    # Configurar la ventana principal
-    root.title("")  # Título de la ventana
-    window_width = 300  # Ancho de la ventana
-    window_height = 200  # Alto de la ventana
-
-    # Obtener el tamaño de la pantalla
-    screen_width = root.winfo_screenwidth()
-    screen_height = root.winfo_screenheight()
-
-    # Calcular las coordenadas x e y para centrar la ventana
-    x_coordinate = (screen_width // 2) - (window_width // 2)
-    y_coordinate = (screen_height // 2) - (window_height // 2)
-
-    # Configurar la geometría de la ventana
-    root.geometry(f"{window_width}x{window_height}+{x_coordinate}+{y_coordinate}")
-
-    # Opcional: evitar redimensionar la ventana
-    root.resizable(False, False)
-
-    # Agregar widgets
-    label = tk.Label(root, text="Bienvenido al transcriptor!")  # Crear una etiqueta
-    label.pack(pady=20)  # Mostrar la etiqueta con un espacio de 20 píxeles
-
-    button_transcriptor = tk.Button(root, text="Elegir audio", command=transcribir)  # Crear un botón para cerrar la ventana
-    button_transcriptor.pack(pady=10)
-    button = tk.Button(root, text="Cerrar", command=root.destroy)  # Crear un botón para cerrar la ventana
-    button.pack(pady=10)
-
-    # Iniciar el bucle principal de la aplicación
-    root.mainloop()
-    
-    """
-    
 
 if __name__ == "__main__":
     main()
